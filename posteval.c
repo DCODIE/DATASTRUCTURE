e530a24160b4aeb33def8a6352805482f018309a
DCODIE <djdimplejd@gmail.com>
2017-06-16 16:02:04 +0530

    C_file

---
+++ posteval.c
@@ -0,0 +1,46 @@
+//Write a C program to evaluate a given postfix expression using a stack.
+
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+#include<string.h>
+#include<ctype.h>
+double compute(char symbol,int op1,int op2)
+ {
+  switch(symbol)
+   {
+    case'+': return op1+op2;
+    case'-': return op1-op2;
+    case'*': return op1*op2;
+    case'/': return op1/op2;
+    case'$':
+    case'^': return pow(op1,op2);
+    default: printf("\nInvalid Input\n");
+   }
+ }
+int main()
+ {
+  double s[20],res,op1,op2;
+  int top,i;
+  char postfix[20],symbol;
+  printf("\nEnter the postfix expression\n");
+  scanf("%s",postfix);
+  top=-1;
+  for(i=0;i<strlen(postfix);i++)
+   {
+    symbol=postfix[i];
+    if(isdigit(symbol))
+      s[++top]=symbol-'0';
+    else
+     {
+      op2=s[top--];
+      op1=s[top--];
+      res=compute(symbol,op1,op2);
+      s[++top]=res;
+     }
+   }
+  res=s[top--];
+  printf("\nThe result is %f \n",res);
+  getch();
+  return 0;
+ }
